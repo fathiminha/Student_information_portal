311a56f984bf1b822a9060d05475d0a3863f976a
Fathima Minha <139671210+fathiminha@users.noreply.github.com>
2024-11-19 15:53:40 +0000

    student data is passed

--- studentapp/src/App.js
+++ studentapp/src/App.js
@@ -1,11 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { students } from './data/StudentsDb';
+import StudentTable from './components/StudentTable';
 
 function App() {
   return (
     <div className="App">
      <h1> Student Information Portal </h1>
      <hr/>
+     <table> 
+      <tbody>
+        <tr>
+          <td> <StudentTable students={students}  /> </td>
+        </tr>
+      </tbody>
+     </table>
     </div>
   );
 }
