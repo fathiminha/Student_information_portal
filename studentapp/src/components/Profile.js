7f6a1b7da7b3465f1179faf1c9016c888801a785
Fathima Minha <139671210+fathiminha@users.noreply.github.com>
2024-11-22 09:24:04 +0000

    added profile layout and codes

--- studentapp/src/components/Profile.js
+++ studentapp/src/components/Profile.js
@@ -1,7 +1,29 @@
-export default function Profile(){
+export default function Profile(props){
     return(
-        <div>
-            
-        </div>
+        <>
+            <table>
+                <tr>
+                    <td rowSpan={7}>
+                        <img className="profile" src={requestAnimationFrame('../assests/img/'+props.stu.profilePic)} alt="profile picture" />
+                    </td>
+                    <td> ID {props.stu.studentId} </td>
+                </tr>
+                <tr>
+                    <td> Full name:{props.stu.firstName+ " "+ props.stu.lastName} </td>
+                </tr>
+                <tr>
+                    <td> Age: {props.stu.age} </td>
+                </tr>
+                <tr>
+                    <td> Course: {props.stu.course} </td>
+                </tr>
+                <tr>
+                    <td> Address: {props.stu.address.city+ ","+props.stu.address.country} </td>
+                </tr> 
+                <tr>
+                    <td> Skills: {props.stu.skills.map(skill=>skill+",")} </td>
+                </tr>
+            </table>  
+        </>
     )
 }
