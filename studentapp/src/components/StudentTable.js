db03af3e3300aba989d24a140d74a25ec0ff3b08
Fathima Minha <139671210+fathiminha@users.noreply.github.com>
2024-11-22 09:27:29 +0000

    view button onclick function added

--- studentapp/src/components/StudentTable.js
+++ studentapp/src/components/StudentTable.js
@@ -25,7 +25,7 @@ export default function StudentTable(props){
                                     <td> {student.lastName} </td>
                                     <td> {student.course} </td>
                                     <td> {student.address.country}  </td>
-                                    <td> <button> View </button> </td>
+                                    <td> <button onClick={()=>setStu(student)}> View </button> </td>
                                 </tr>
                             )
                         }
