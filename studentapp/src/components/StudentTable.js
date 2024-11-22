48b8f1480e0efb5a53882f934fa91f5258de5325
Fathima Minha <139671210+fathiminha@users.noreply.github.com>
2024-11-22 09:02:11 +0000

    profile component is created

--- studentapp/src/components/StudentTable.js
+++ studentapp/src/components/StudentTable.js
@@ -1,5 +1,9 @@
 import '../assests/css/compo.css'
+import Profile from './Profile'
+import {useState} from 'react';
+
 export default function StudentTable(props){
+    const [stu,setStu] = useState(props.students[0]);
     return(
         <div class="outerDiv">
             <div class="leftDiv">
@@ -10,6 +14,7 @@ export default function StudentTable(props){
                             <td> Last Name </td>
                             <td> Course </td>
                             <td> Country </td>
+                            <td> Profile </td>
                         </tr>
                     </thead>
                     <tbody>
@@ -20,11 +25,15 @@ export default function StudentTable(props){
                                     <td> {student.lastName} </td>
                                     <td> {student.course} </td>
                                     <td> {student.address.country}  </td>
+                                    <td> <button> View </button> </td>
                                 </tr>
                             )
                         }
                     </tbody>
                 </table>
+            </div>
+            <div className="rightDiv">
+                <Profile student={stu} />
             </div>
 
         </div>
